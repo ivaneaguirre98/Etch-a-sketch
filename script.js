d0028b5019054337afa281ebc29c40762d391e91
Ivan <aguirreivan96@gmail.com>
2021-08-12 11:30:45 -0500

    Added a clear button to create a fresh new grid

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const container = document.querySelector('#container');
 let defaultSquare = 16;
 let squareNum = document.getElementById("numInput").value = 16;
 
-//function that sets width a
-
 //creates div and adds class of gridSquare
 function createGrid(col, rows){
     for(let i = 0; i < (col * rows); i++){
@@ -17,6 +15,9 @@ function createGrid(col, rows){
     //this is the event listener to fill in the squares when you hover over them
     var gridPixels = container.querySelectorAll('div');
     gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', fillGrid));
+
+    let clearButton = document.getElementById("clearButton");
+    clearButton.addEventListener("click", clearGrid);
 
     //event listeners for creating a new grid
     let newGridButt = document.getElementById("newGridButt");
